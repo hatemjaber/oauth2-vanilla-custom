a2452d292757e46ddf398c0e37ccdae44164e501
hjaber <hatem@7079.net>
2015-10-09 11:16:41 -0400

    first commit

---
+++ authserver/src/main/java/demo/AuthserverApplication.java
@@ -0,0 +1,82 @@
+package demo;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableResourceServer
+@EnableWebSecurity
+public class AuthserverApplication extends WebSecurityConfigurerAdapter {
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		 auth.userDetailsService(userDetailsService);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserverApplication.class, args);
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+	@Bean
+	public InMemoryTokenStore inMemoryTokenStoreBean() {
+		return new InMemoryTokenStore();
+	}
+	
+	@Configuration
+	@EnableAuthorizationServer
+	protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+		@Autowired
+		@Qualifier("authenticationManagerBean")
+		private AuthenticationManager authenticationManager;
+		
+		@Override
+		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+			endpoints.authenticationManager(authenticationManager);
+		}
+		
+		@Override
+		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+			clients.inMemory()
+					.withClient("acme")
+					.secret("acmesecret")
+					.authorizedGrantTypes("authorization_code", "refresh_token",
+							"password").scopes("openid").autoApprove(true);
+		}
+
+	}
+
+}
---
+++ authserver/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into user(id, name, login, password) values (1,'Roy','roy','spring');
+insert into user(id, name, login, password) values (2,'Craig','craig','spring');
+insert into user(id, name, login, password) values (3,'Greg','greg','spring');
+insert into user(id, name, login, password) values (4,'Hatem','user','password');
